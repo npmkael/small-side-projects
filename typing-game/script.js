77b58022850d2d94300140ad7cb77ae4acd1b341
kielcodescode30 <kielycarreon123@gmail.com>
2024-07-26 06:58:52 +0800

    added focus message and fetch random quotes in local JSON

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,41 +1,8 @@
 "use strict";
 let getQuotes;
 let randomNum;
-const textDisplay = document.querySelector("[data-text-display]");
-const textInputElement = document.getElementById("textInput");
 
-// TODO:
-// Gamemode
-// Timer
-// Replace Textarea
-
-textInputElement.addEventListener("input", () => {
-  const arrTexts = textDisplay.querySelectorAll("span");
-  const arrayValue = textInputElement.value.split("");
-
-  let correct = true;
-  arrTexts.forEach((characterSpan, index) => {
-    const character = arrayValue[index];
-    console.log(character);
-    if (character === undefined) {
-      characterSpan.classList.remove("correct");
-      characterSpan.classList.remove("incorrect");
-      correct = false;
-    } else if (character === characterSpan.innerText) {
-      characterSpan.classList.add("correct");
-      characterSpan.classList.remove("incorrect");
-    } else {
-      characterSpan.classList.remove("correct");
-      characterSpan.classList.add("incorrect");
-      correct = false;
-    }
-  });
-
-  if (correct) {
-    console.log("working");
-    getNextNote();
-  }
-});
+const words = document.getElementById("words");
 
 function getRandomQuote() {
   return fetch("random-quotes.json")
@@ -46,15 +13,14 @@ function getRandomQuote() {
 async function getNextNote() {
   const quotes = await getRandomQuote();
   randomNum = Math.floor(Math.random() * quotes.length);
-  textDisplay.innerHTML = "";
-  console.log(randomNum);
   let quote = quotes[randomNum].quote;
+
   quote.split("").forEach((character) => {
     const characterSpan = document.createElement("span");
     characterSpan.innerText = character;
-    textDisplay.appendChild(characterSpan);
+
+    words.appendChild(characterSpan);
   });
-  textInputElement.value = null;
 }
 
 getNextNote();
